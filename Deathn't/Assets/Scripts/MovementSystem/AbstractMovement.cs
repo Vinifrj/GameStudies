3090329671005d1060f49529487a22cb80483d10
Guilherme Alves Lindo <guilherme.alves.lindo@outlook.com>
2021-11-04 13:57:34 -0300

    fixed player stuck on the wall
    Fixed player stuck on the wall pre-checking its horizontal collision. Needs to be verified if it does not broke other objects behavior.

--- Deathn't/Assets/Scripts/MovementSystem/AbstractMovement.cs
+++ Deathn't/Assets/Scripts/MovementSystem/AbstractMovement.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public abstract class AbstratMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Vector2 velocityInput { get; protected set; }
     public bool jumpInput { get; protected set; }
+    
+    [SerializeField]
+    private CapsuleCollider2D CapsuleCollider;
+
     [SerializeField]
     private UpdateType updateType = UpdateType.LateUpdate;
     public bool holdSimulation = false;
@@ -26,6 +31,37 @@ public abstract class AbstratMovement : MonoBehaviour
     public virtual void Move(Vector2 input)
     {
         velocityInput = input;
+    }
+
+    public virtual void HorizontalMove(float input)
+    {
+        Debug.Log(CapsuleCollider.size);
+        RaycastHit2D hit;
+        // horizontal input.
+        
+
+        if(input > 0)
+            hit = Physics2D.Raycast(rb.position+new Vector2(+CapsuleCollider.size.x/2,-CapsuleCollider.size.y/2),Vector2.right);
+        else
+            hit = Physics2D.Raycast(rb.position+new Vector2(-CapsuleCollider.size.x/2,-CapsuleCollider.size.y/2),Vector2.left);
+
+        //if(input > 0)
+        //    hit = Physics2D.Raycast(rb.position+new Vector2(+0.176f,-0.35f),Vector2.right);
+        //else
+        //    hit = Physics2D.Raycast(rb.position+new Vector2(-0.176f,-0.35f),Vector2.left);
+        if (hit.collider != null)
+        {
+            if(hit.distance < 0.03f && hit.distance > -0.03f)
+                velocityInput = new Vector2(0f,velocityInput.y);
+            else
+                velocityInput = new Vector2(input,velocityInput.y);
+        }
+        else
+        {
+            velocityInput = new Vector2(input,velocityInput.y);
+        }
+        //Debug.Log(hit.distance);
+
     }
 
     // returns true if the character jumped 
@@ -59,7 +95,7 @@ public abstract class AbstratMovement : MonoBehaviour
 
     public void ClearInputs()
     {
-        velocityInput = Vector2.zero;
+        //velocityInput = Vector2.zero;
         jumpInput = false;
     }
 
